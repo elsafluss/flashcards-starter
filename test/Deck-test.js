64c6375589def42d8bb0d56fec34982cbc6c5d28
Elsa Fluss <elsafluss@gmail.com>
2020-12-03 19:32:43 -0700

    Made deck-test tests pass.

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -18,21 +18,18 @@ describe('Deck', function () {
   })
 
   it('should have an array of cards', function () {
-    const deck = new Deck()
     const card1 = new Card(1, 'What is my name', ['Elsa', 'Matt', 'Gonzo'], 'Elsa')
     const card2 = new Card(2, 'What is your name', ['Elsa', 'Matt', 'Gonzo'], 'Matt')
-    deck.createDeck(card1)
-    deck.createDeck(card2)
-    expect(deck.cards).to.be.an('array')
-    expect(deck.cards[0]).to.deep.equal(card1)
-    expect(deck.cards[1]).to.deep.equal(card2)
+    const deck = new Deck([card1, card2])
+    expect(deck.currentDeck).to.be.an('array')
+    expect(deck.currentDeck[0]).to.deep.equal(card1)
+    expect(deck.currentDeck[1]).to.deep.equal(card2)
   })
 
   it('should know how many cards there are', function () {
     const card = new Card(1, 'What is my name', ['Elsa', 'Matt', 'Gonzo'], 'Elsa')
-    const deck = new Deck()
-    deck.createDeck(card)
-    expect(deck.countCards()).to.equal(deck.cards[0].length)
+    const deck = new Deck([card])
+    expect(deck.countCards()).to.equal(deck.currentDeck.length)
   })
 
 })
